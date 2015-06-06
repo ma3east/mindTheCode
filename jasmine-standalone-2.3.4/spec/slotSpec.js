b9731395d1905acd68955fbd86826680945d0a84
Andrea Mckenzie <ma3east@gmail.com>
2015-06-06 19:55:58 +0100

    trying to progress with the tests

--- jasmine-standalone-2.3.4/spec/slotSpec.js
+++ jasmine-standalone-2.3.4/spec/slotSpec.js
@@ -6,40 +6,45 @@ describe("Play Button", function() {
 			var slot = new Slot();
 			var startImage = slot.getVisibleImage();
 			
-			oneIteration();
+			slot.oneIteration();
+			
+			var endImage = slot.getVisibleImage(); 
+			
+			expect(endImage).not.toEqual(startImage);
+		});
+		
+	});
+		
+	describe("The process if iteration surpasses number of images", function() {
+		it("should still select an image", function() {
+			
 			
 			var endImage = slot.getVisibleImage(); 
 			
 			expect(endImage).not.toEqual(startImage);
 
-
-		});	
-	
-	
+		});
 	});
+		
+	
 	
 	it("should start spinning the animations", function() {
 
 
-
 	});
+	
 	it("should start the giving spin for a specified time", function() {
 
 
-
 	});
+	
 	it("should stop the spinning", function() {
-
 
 
 	});
 
 	it("should select the image", function() {
 
-
-
 	});
 		
 });
-	
-	
--- jasmine-standalone-2.3.4/src/slot.js
+++ jasmine-standalone-2.3.4/src/slot.js
@@ -1,13 +1,28 @@
+
+// this is the slot class, denotates how all slots behave
 function Slot (){
 	
+	this.available = ["apple", "orange", "pear"];
+	this.pose = this.available[0];
 	this.getVisibleImage = function () {
 	
-	
+		return this.pose;
 	};
+	
+	this.oneIteration = function () {
+		
+		var currentIndex = this.available.indexOf(this.pose);
+		
+		this.pose = this.available[currentIndex + 1];
+		
+	}
 
 	
 }
 
-function oneIteration () {
-	
-}
+
+
+
+
+// this an instance of a slot
+var slot = new Slot();
